d4dd2f967f2701bef0dd6dcbed1b455cbaf9a03f
Tanawat Suriyachai <mts131415@gmail.com>
2021-09-16 11:49:37 +0700

    First version
    First commit with web-app & smart contract

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# simple-payment
+<h1 align="center">
+  Simple Payment with Solidity
+</h1>
+
+<p align="center"><img src="./docs/screen-recording.gif" alt="screen-recording gif" /></p>
+
+This repository is part of `University of the Thai Chamber of Commerce`, `Master of Engineering Program in Computer Engineering and Financial Technology`, Final Exam on `NF517 - Advanced Financial Technology (Blockchain/DApp)`
+
+## 🧐 Features
+
+Build with `React JS` and connect to `Smart Contract` 
+
+- **Product List**
+
+- **Add to Cart**
+
+- **Checkout Order**
+
+- **Order List**
+
+## 🛠️ Installation Steps
+
+1. Clone the repository
+
+```bash
+git clone https://github.com/2030731303010/simple-payment.git
+```
+
+2. Change the working directory to your web-server directory
+
+3. Try to access `index.html` via you web-browser with
+
+```bash
+http://you-local-host:8080
+```
+
+🌟 You are all set!
+
+## 💻 Built with
+- [React JS](https://reactjs.org/)
+- [Material UI](https://material-ui.com/): for styling
+- [Remix](https://remix.ethereum.org/): for building `Smart Contract`
+
+
+##  Group members
+
+- 2030731303002 - ศุภปภา รุ่งเจริญรวยยิ่ง
+- 2030731303010 - ธนวัฒน์ สุริยะฉาย
+- 2110731303003 - เศรษฐกาล ชื่นโกมล
+- 2110731303005 - มงคล ศรีช้าง
+- 2110731303007 - ปิยัคพัฒน์ สุขสาร
+
+<hr>
+<p align="center">
+Developed with ❤️ in Thailand 🇹🇭 
+</p>
---
+++ precache-manifest.c438f3ed1b12021868eddc1637df3da3.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbd70980580a8ad17e4bb8bbc1e5395c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "247b3e19d95a194c297d",
+    "url": "/static/css/4.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "ef898499779fcd71630e",
+    "url": "/static/css/main.67ff5043.chunk.css"
+  },
+  {
+    "revision": "60a616efaf633e074d99",
+    "url": "/static/js/0.61e64db2.chunk.js"
+  },
+  {
+    "revision": "9feea35c529b3ea3285c",
+    "url": "/static/js/1.7e9cb864.chunk.js"
+  },
+  {
+    "revision": "11dcb98311c786f0baf038cd484627ad",
+    "url": "/static/js/1.7e9cb864.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3707f3a10681063e94e9",
+    "url": "/static/js/10.0d1d9cb3.chunk.js"
+  },
+  {
+    "revision": "3d816b0e083325c9b338",
+    "url": "/static/js/11.11b27b06.chunk.js"
+  },
+  {
+    "revision": "aa966f11c869204e1fbd",
+    "url": "/static/js/12.f3d0bf57.chunk.js"
+  },
+  {
+    "revision": "224c2b311bfbe6c1c48b",
+    "url": "/static/js/13.ef81acb3.chunk.js"
+  },
+  {
+    "revision": "660f090de59e909c673d",
+    "url": "/static/js/14.61a71869.chunk.js"
+  },
+  {
+    "revision": "247b3e19d95a194c297d",
+    "url": "/static/js/4.5d2ec687.chunk.js"
+  },
+  {
+    "revision": "17e2b8ec7db80df94908d18e52fe7798",
+    "url": "/static/js/4.5d2ec687.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5069580a49350341343",
+    "url": "/static/js/5.b95d1062.chunk.js"
+  },
+  {
+    "revision": "e4a0d87becbcc6d09668875fe197b50f",
+    "url": "/static/js/5.b95d1062.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "497f7064d2faafd4d5dd",
+    "url": "/static/js/6.600c0c8a.chunk.js"
+  },
+  {
+    "revision": "cfab52740f49d25b5db2",
+    "url": "/static/js/7.b270b9e5.chunk.js"
+  },
+  {
+    "revision": "0dff594f7aa9b739ed9d",
+    "url": "/static/js/8.4025cf42.chunk.js"
+  },
+  {
+    "revision": "23a157ba8a8d52f8dba2",
+    "url": "/static/js/9.4f7a7c3e.chunk.js"
+  },
+  {
+    "revision": "ef898499779fcd71630e",
+    "url": "/static/js/main.017fe02f.chunk.js"
+  },
+  {
+    "revision": "3198b7c5f68956bce404",
+    "url": "/static/js/runtime-main.be3c88b7.js"
+  },
+  {
+    "revision": "16b4ce72cf30da14d83d5e3981d3113c",
+    "url": "/static/media/Prompt-Regular.16b4ce72.ttf"
+  }
+]);
